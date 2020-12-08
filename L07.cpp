2afd60d721037113e727f4c468ab6b9000be49bd
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-12-08 11:41:21 +0100

    7a->binar,doskonala - dziala

--- L07.cpp
+++ L07.cpp
@@ -6,28 +6,67 @@ using namespace std;
 
 #ifdef Zadanie7A
 
-bool ldoskonala(int a) {
+bool ldoskonala(int &refa) {
     
     int suma = 0;
-    for (int i = 1; i <= a/2; i++) {
-        if (a % i == 0) 
+    for (int i = 1; i <= refa/2; i++) {
+        if (refa % i == 0) 
             suma += i;
     }
-    if (suma == a) {
-        return 1;
-        cout <<"Liczba jest doskonala";
+    if (suma == refa) {
+        return true;
+        cout <<"Liczba jest doskonala"<<endl;
     }
     else {
-        return 0;
-        cout <<"Liczba nie jest doskonala";
+        return false;
+        cout <<"Liczba nie jest doskonala"<<endl;
     }
 }
 
+/*int potegowanie(int x, int n) {
+    
+    if(n == 0) {
+        return 1;
+    }
+    else {
+        int zmienna = potegowanie(x, n--);
+        return x * zmienna;
+    }
+}*/
 
+void binar(int &w) {
+    int i = 0;
+    int tab[100] = {};
+    if (w < 0)
+        cout << "BLAD";
+    else {
+        while (w) {
+            tab[i++] = w % 2;
+            w = w / 2;
+        }
+    }
+    for (int j = i-1; j >= 0; j--)
+        cout << tab[j];
+    cout << endl;
+}
 
 int main()
 {
-    ldoskonala(6);
+    int a,b;
+
+    cout << "Podaj liczbe i sprawdz czy jest doskonala -> ";
+    cin >> a;
+    cout << ldoskonala(a) << endl;
+    
+    /*cout << "Podaj podstawe potegi -> ";
+    cin >> a;
+    cout << "Podaj wykladnik potegi -> ";
+    cin >> b;
+    potegowanie(a,b);
+    */
+    cout << "Podaj liczbe dziesietna ktora chcesz zamienic na liczbe binarna -> ";
+    cin >> a;
+    binar(a);
 }
 #endif // Zadanie7A
 
