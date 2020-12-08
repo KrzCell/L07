e91a13e0ca4c4ac6220de4c1d225fc250c0f4b54
Krzysztof Cel <krzycel789@student.polsl.pl>
2020-12-08 10:20:13 +0100

    7a - doskonala

--- L07.cpp
+++ L07.cpp
@@ -5,9 +5,29 @@ using namespace std;
 
 
 #ifdef Zadanie7A
+
+bool ldoskonala(int a) {
+    
+    int suma = 0;
+    for (int i = 1; i <= a/2; i++) {
+        if (a % i == 0) 
+            suma += i;
+    }
+    if (suma == a) {
+        return 1;
+        cout <<"Liczba jest doskonala";
+    }
+    else {
+        return 0;
+        cout <<"Liczba nie jest doskonala";
+    }
+}
+
+
+
 int main()
 {
-    
+    ldoskonala(6);
 }
 #endif // Zadanie7A
 
